19f60619f576ce0319a0fffef4a8d792dbc04d96
Erickinhou <willianerick17@gmail.com>
2021-08-05 15:22:44 -0300

    Fix tailwind next cache bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+    './public/**/*.html',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
