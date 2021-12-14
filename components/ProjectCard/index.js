552a6936994b6c72b24bece6564460240bb41eb0
Erickinhou <willianerick17@gmail.com>
2021-12-14 19:34:44 -0300

    Add bencare project

--- components/ProjectCard/index.js
+++ components/ProjectCard/index.js
@@ -1,24 +1,41 @@
-import { Title, Subtitle } from '../Typografy';
+import { Title, Subtitle } from '../Typografy'
+import { useRouter } from 'next/router'
+const projects = [
+  {
+    image: '/images/projects/spread.png',
+    title: 'Spread Software',
+    link: 'https://app.spread.software/login',
+    subtitle: `It's the best transcription platform ever. The platform allows you to
+    take a video and transform to text. You can change the text at real
+    time when you see the video. we use typescript, react and firebase`,
+  },
+  {
+    image: '/images/projects/bentcare.png',
+    title: 'Bentcare',
+    link: 'https://bentcare.com.br/',
+    subtitle: `Bentcare has arrived to transform access to healthcare. Through a digital, modern and versatile platform, it connects healthcare professionals to their patients in a faster, safer, more economical and humanized way. Forget the traditional way of meeting and scheduling appointments and exams. Welcome to the reality of those who really want your well...`,
+  },
+]
 
 export default function ProjectCard(props) {
-  return (
+  const router = useRouter()
+
+  return projects.map((project) => (
     <>
-      <section className="py-6">
+      <section
+        className="py-6 cursor-pointer"
+        onClick={() => router.push(project.link)}
+      >
+        <Title small>{project.title}</Title>
+        <Subtitle small>{project.subtitle}</Subtitle>
         <div className="w-full p-3">
           <img
             className="w-full h-full object-cover"
-            src="/images/projects/spread.png"
+            src={project.image}
             alt="spread-project"
           />
         </div>
-
-        <Title small>Spread Software</Title>
-        <Subtitle small>
-          It's the best transcription platform ever. The platform allows you to
-          take a video and transform to text. You can change the text at real
-          time when you see the video. we use typescript, react and firebase
-        </Subtitle>
       </section>
     </>
-  );
+  ))
 }
