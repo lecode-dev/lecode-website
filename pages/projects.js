3fdf04029a8880c43599e51ccaeb1ed4ac659808
Erickinhou <willianerick17@gmail.com>
2021-12-21 13:42:53 -0300

    Add Footer

--- pages/projects.js
+++ pages/projects.js
@@ -4,23 +4,27 @@ import { Subtitle } from '../components/Typografy'
 import Navbar from '../components/Navbar'
 import ProjectCard from '../components/ProjectCard'
 import Divider from '../components/Divider'
+import Footer from '../components/Footer'
 
 export default function projects() {
   return (
-    <main>
-      <Head>
-        <title>lecode - Projects</title>
-      </Head>
-      <Navbar />
-      <div className="relative pt-12">
-        <Title>Our Projects</Title>
-        <Subtitle>
-          We think different, you can see below some of our projects made with
-          love.
-        </Subtitle>
-      </div>
-      <Divider>Projects</Divider>
-      <ProjectCard />
-    </main>
+    <>
+      <main>
+        <Head>
+          <title>lecode - Projects</title>
+        </Head>
+        <Navbar />
+        <div className="relative pt-12">
+          <Title>Our Projects</Title>
+          <Subtitle>
+            We think different, you can see below some of our projects made with
+            love.
+          </Subtitle>
+        </div>
+        <Divider>Projects</Divider>
+        <ProjectCard />
+      </main>
+      <Footer />
+    </>
   )
 }
