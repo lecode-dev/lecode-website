f445aeb56ad7abb4554ac2ac311b17ee017154eb
Erickinhou <willianerick17@gmail.com>
2021-12-21 13:44:43 -0300

    Add blog Head

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,13 +1,16 @@
-import Navbar from '../../components/Navbar';
-import Container from '../../components/Container';
+import Navbar from '../../components/Navbar'
+import Container from '../../components/Container'
 
 export default function Blog() {
   return (
     <>
+      <Head>
+        <title>lecode - Blog</title>
+      </Head>
       <Navbar />
       <Container verticalSpace={true}>
         <h1 className="text-center">It's comming :)</h1>
       </Container>
     </>
-  );
+  )
 }
