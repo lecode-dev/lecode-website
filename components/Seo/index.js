ef2939f9691ceb9c154333cb3a98f99cbf98e308
Erickinhou <willianerick17@gmail.com>
2022-02-07 12:25:00 -0300

    change meta tags

--- components/Seo/index.js
+++ components/Seo/index.js
@@ -4,32 +4,28 @@ export const SeoTags = ({
   title = 'lecode',
   description = 'We are a software house and a agency specialized in building websites and applications.',
 }) => {
-  const urlImage = 'https://lecode.dev/images/lecode-seo.WebP'
+  const urlImage = 'http://lecode.dev/images/lecode-seo-300.WebP'
   return (
     <Head>
       <title>{title}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <meta name="description" content={description} />
-      {/* Twitter */}
-      <meta name="twitter:card" content="summary" key="twcard" />
-      <meta name="twitter:creator" content="@lecodedev" key="twhandle" />
 
       {/* Open Graph */}
-      <meta property="og:url" content="https://lecode.dev/" key="ogurl" />
-      <meta
-        property="og:image:secure_url"
-        itemprop="image"
-        content={urlImage}
-        key="ogimage"
-      />
+      <meta property="og:url" content="https://lecode.dev/" />
+      <meta property="og:image" itemprop="image" content={urlImage} />
       <meta property="og:image:type" content="image/WebP" />
       <meta property="og:image:width" content="300" />
       <meta property="og:image:height" content="300" />
       <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="Lecode" key="ogsitename" />
-      <meta property="og:title" content={title} key="ogtitle" />
-      <meta property="og:description" content={description} key="ogdesc" />
+      <meta property="og:site_name" content="Lecode" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content="@lecodedev" />
     </Head>
   )
 }
