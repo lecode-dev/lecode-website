bd2c8e16fe561df2ba1fb9ea2b893fa2a7bc5797
Erickinhou <willianerick17@gmail.com>
2022-02-07 12:14:29 -0300

    Update Seo tags

--- components/Seo/index.js
+++ components/Seo/index.js
@@ -2,9 +2,9 @@ import Head from 'next/head'
 
 export const SeoTags = ({
   title = 'lecode',
-  description = 'We are a software house and a agency specialized in building modern, exceptional, high-quality websites and applications.',
+  description = 'We are a software house and a agency specialized in building websites and applications.',
 }) => {
-  const urlImage = 'https://lecode.dev/images/team/lecode.jpeg'
+  const urlImage = 'https://lecode.dev/images/lecode-seo.WebP'
   return (
     <Head>
       <title>{title}</title>
@@ -17,7 +17,13 @@ export const SeoTags = ({
 
       {/* Open Graph */}
       <meta property="og:url" content="https://lecode.dev/" key="ogurl" />
-      <meta property="og:image" content={urlImage} key="ogimage" />
+      <meta
+        property="og:image:secure_url"
+        itemprop="image"
+        content={urlImage}
+        key="ogimage"
+      />
+      <meta property="og:type" content="website" />
       <meta property="og:site_name" content="Lecode" key="ogsitename" />
       <meta property="og:title" content={title} key="ogtitle" />
       <meta property="og:description" content={description} key="ogdesc" />
