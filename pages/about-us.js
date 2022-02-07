613d53259d8c901f56ec94212511593d1c680ac4
Erickinhou <willianerick17@gmail.com>
2022-02-07 14:17:18 -0300

    Remove Seo component

--- pages/about-us.js
+++ pages/about-us.js
@@ -2,12 +2,45 @@ import Navbar from '../components/Navbar'
 import TeamSession from '../components/TeamSession'
 import AboutUsSection from '../components/AboutUsSection'
 import Footer from '../components/Footer'
-import { SeoTags } from '../components/Seo'
+import Head from 'next/head'
 
 export default function AboutUs() {
   return (
     <>
-      <SeoTags title="About-Us" />
+      <Head>
+        <title>about-us</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="We are a software house and a agency specialized in building websites and applications."
+        />
+
+        {/* Open Graph */}
+        <meta property="og:url" content="https://lecode.dev/" />
+        <meta
+          property="og:image:secure_url"
+          itemprop="image"
+          content={urlImage}
+        />
+        <link itemprop="thumbnailUrl" href={urlImage} />
+        <meta property="og:image:type" content="image/WebP" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Lecode" />
+        <meta property="og:title" content={title} key="ogtitle" />
+        <meta
+          property="og:description"
+          content="We are a software house and a agency specialized in building websites and applications."
+          key="ogdescription"
+        />
+        <meta property="og:updated_time" content="1440432930" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@lecodedev" />
+      </Head>
       <Navbar />
       <AboutUsSection />
       <TeamSession />
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,47 @@
 import Navbar from '../../components/Navbar'
 import Container from '../../components/Container'
-import { SeoTags } from '../../components/Seo'
+import Head from 'next/head'
 
 export default function Blog() {
   return (
     <>
-      <SeoTags title="Blog" />
+      <Head>
+        <title>blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="We are a software house and a agency specialized in building websites and applications."
+        />
+
+        {/* Open Graph */}
+        <meta property="og:url" content="https://lecode.dev/" />
+        <meta
+          property="og:image:secure_url"
+          itemprop="image"
+          content="http://lecode.dev/images/lecode-seo-300.WebP"
+        />
+        <link
+          itemprop="thumbnailUrl"
+          href="http://lecode.dev/images/lecode-seo-300.WebP"
+        />
+        <meta property="og:image:type" content="image/WebP" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Lecode" />
+        <meta property="og:title" content={title} key="ogtitle" />
+        <meta
+          property="og:description"
+          content="We are a software house and a agency specialized in building websites and applications."
+          key="ogdescription"
+        />
+        <meta property="og:updated_time" content="1440432930" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@lecodedev" />
+      </Head>
       <Navbar />
       <Container verticalSpace={true}>
         <h1 className="text-center">It's comming :)</h1>
