754c4d0c6559cb4a0e6ffab6eef26ae272c91a6e
Erickinhou <willianerick17@gmail.com>
2022-02-25 01:01:31 -0300

    change to fixed top padding

--- components/Home/index.js
+++ components/Home/index.js
@@ -47,7 +47,7 @@ const Home = () => {
             <div className="relative z-10 pb-8 lg:h-screen bg-gray-darkest sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
               <Navbar />
 
-              <main className="mt-2 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+              <main className="mx-auto max-w-7xl px-4 mt-28 sm:px-6 lg:px-8 ">
                 <div className="sm:text-center lg:text-left">
                   <h1 className="text-4xl tracking-tight font-extrabold text-gray-200 sm:text-5xl md:text-6xl">
                     <span className="block xl:inline">{t('title_normal')}</span>{' '}
